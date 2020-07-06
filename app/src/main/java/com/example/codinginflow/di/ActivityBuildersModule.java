c138fd7e004a3ffb9a6582d83d7767080a5269d9
Vasile <vaseoa@yahoo.com>
2020-07-06 16:01:51 +0300

    setup project

---
+++ app/src/main/java/com/example/codinginflow/di/ActivityBuildersModule.java
@@ -0,0 +1,14 @@
+package com.example.codinginflow.di;
+
+import com.example.codinginflow.AuthActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+}
