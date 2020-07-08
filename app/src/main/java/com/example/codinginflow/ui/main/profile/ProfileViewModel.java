2993e2835792714ff9c6cc4c1f23e862a90db413
Vasile <vaseoa@yahoo.com>
2020-07-08 13:39:40 +0300

    insert fragment view model + data

--- app/src/main/java/com/example/codinginflow/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/codinginflow/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.example.codinginflow.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.codinginflow.SessionManager;
+import com.example.codinginflow.models.User;
+import com.example.codinginflow.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,10 +15,15 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "DaggerExample";
 
+    private final SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: viewmodel is ready...");
     }
 
-
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
+    }
 }
